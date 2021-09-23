161c85d61f84ecbd2f2237f3f998489255587b0c
Alexander <a.kulakov.vironit@vironit.ru>
2021-09-23 18:01:31 +0300

    build: rename modules

--- domain/src/main/java/com/example/domain/models/Markdown.kt
+++
@@ -1,8 +0,0 @@
-package com.example.domain.models
-
-data class Markdown(
-    val placeId: String,
-    val name: String? = null,
-    val address: String? = null,
-    val location: Location? = null
-)
